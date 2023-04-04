47e7d25f96e29aa7403bc989c051fe923371da70
iwahiro <y-iwama@nec.comw>
2023-04-04 17:44:53 +0900

    modify components

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.mp3$/,
+      use: {
+        loader: "url-loader",
+        options: {
+          limit: 8192,
+        },
+      },
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,29 @@
-import { Typography } from "@mui/material";
+import React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Header from "../component/Header";
 
-export default function Home() {
+import dynamic from "next/dynamic";
+const Ordering = dynamic(() => import("../component/Ordering"), { ssr: false });
+const theme = createTheme();
+
+function App() {
   return (
-    <>
-      <Typography>home</Typography>
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header />
+      <Container component="main" maxWidth="lg">
+        <Paper
+          variant="outlined"
+          sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
+        >
+          <Ordering />
+        </Paper>
+      </Container>
+    </ThemeProvider>
   );
 }
+
+export default App;
