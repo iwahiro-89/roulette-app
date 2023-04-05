f93f6217f42aa372bc51cf2e715ecc80d575fb49
iwahiro <y-iwama@nec.comw>
2023-04-05 10:22:21 +0900

    fix build error

---
+++ dist/static/1T_2RE6w6yX9F-mUIbOha/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/287-e688e11469fb34bf.js","static/chunks/pages/index-90ffcb8c6b442b2b.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
